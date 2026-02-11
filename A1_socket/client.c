fd57dc06766539738096d0c225129de50f573b44
Rajvir Singh <rajvirsi@buffalo.edu>
2026-02-11 14:36:02 -0500

    "Complete A1 socket programming assignment"

--- A1_socket/client.c
+++ A1_socket/client.c
@@ -26,10 +26,65 @@ int main(int argc, char *argv[]) {
     }
 
     // TODO: Create a TCP socket (AF_INET, SOCK_STREAM).
+    int client_id = socket(AF_INET, SOCK_STREAM,0);
+    if(client_id < 0){
+        perror("socket");return 1;
+    }
     // TODO: Populate sockaddr_in with server IP/port.
+    struct sockaddr_in server_address;
+    memset(&server_address,0 , sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons((uint16_t)port_long);
+    int rc = inet_pton(AF_INET, argv[1], &server_address.sin_addr);
+    if(rc==0){
+        fprintf(stderr,"Invalid IP address : %s\n", argv[1]);
+        close(client_id);return 1;
+    }else if(rc <0 ){
+        perror("inet_pton");
+        close(client_id);return 1;
+    }
     // TODO: Connect to the server.
+    if(connect(client_id,(struct sockaddr *)&server_address,sizeof(server_address)) < 0){
+        perror("connect");
+        close(client_id);
+        return 1;
+    }
 
     // TODO: Read from stdin in a loop (read()) and send in chunks.
+    enum { BUFFER_SIZE = 4096} ;
+    char buffer[BUFFER_SIZE];
+
+    for (;;) {
+        ssize_t num_read = read(STDIN_FILENO, buffer, sizeof(buffer));
+        if (num_read == 0) {
+            break;
+        }
+        if (num_read < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            perror("read");
+            close(client_id);
+            return 1;
+        }
+
+        ssize_t total_sent = 0;
+        while (total_sent < num_read) {
+            ssize_t num_sent = send(client_id,
+                                 buffer + total_sent,
+                                 (size_t)(num_read - total_sent),
+                                 0);
+            if (num_sent < 0) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                perror("send");
+                close(client_id);
+                return 1;
+            }
+            total_sent += num_sent;
+        }
+    } 
     // TODO: For each chunk, send the *exact bytes* you read.
     //   - Use send()/write() in a loop to handle partial sends.
     //   - Do NOT add newlines, prefixes, or other formatting.
@@ -37,5 +92,9 @@ int main(int argc, char *argv[]) {
 
     // TODO: Close the socket before exiting.
 
+    if(close(client_id) <0 ){
+        perror("close");
+        return 1;
+    }
     return 0;
 }
--- A1_socket/server.c
+++ A1_socket/server.c
@@ -26,9 +26,33 @@ int main(int argc, char *argv[]) {
     }
 
     // TODO: Create a TCP listen socket (AF_INET, SOCK_STREAM).
+    int listen_id = socket(AF_INET, SOCK_STREAM, 0);
+    if(listen_id < 0){
+        perror("socket");
+        return 1;
+    }
     // TODO: Set SO_REUSEADDR on the listen socket.
+    int yes = 1;
+    if(setsockopt(listen_id,SOL_SOCKET,SO_REUSEADDR,&yes, sizeof(yes)) < 0 ){
+        perror("setsockopt");
+        close(listen_id);
+        return 1;
+    }
+    
     // TODO: Bind the socket to INADDR_ANY and the given port.
     // TODO: Listen with a small backlog (e.g., 5-10).
+
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons((uint16_t)port_long);
+
+    if (bind(listen_id,(struct sockaddr *)&server_address,sizeof(server_address)) < 0) {
+        perror("bind");
+        close(listen_id);
+        return 1;
+    }
 
     // TODO: Accept clients in an infinite loop.
     //   - For each client, read in chunks until EOF.
@@ -37,8 +61,60 @@ int main(int argc, char *argv[]) {
     //   - Do NOT use printf/fputs or add separators/newlines/prefixes.
     //   - The test harness compares server stdout byte-for-byte with client input.
     // TODO: Handle EINTR and other error cases as specified.
+        if (listen(listen_id, 10) < 0) {
+        perror("listen");
+        close(listen_id);
+        return 1;
+    }
 
     // TODO: Close the listen socket before exiting.
+        enum { BUFFER_SIZE = 4096 };
+        char buffer[BUFFER_SIZE];
+
+    for (;;) {
+        int client_id = accept(listen_id, NULL, NULL);
+        if (client_id < 0) {
+            if (errno == EINTR) {
+                continue;
+            }
+            perror("accept");
+            break; 
+        }
+
+        for (;;) {
+            ssize_t nread = read(client_id, buffer, sizeof(buffer));
+            if (nread == 0) {
+                break;
+            }
+            if (nread < 0) {
+                if (errno == EINTR) {
+                    continue;
+                }
+                perror("read");
+                break; 
+            }
+
+            ssize_t total_written = 0;
+            while (total_written < nread) {
+                ssize_t nwritten = write(STDOUT_FILENO,
+                                         buffer + total_written,
+                                         (size_t)(nread - total_written));
+                if (nwritten < 0) {
+                    if (errno == EINTR) {
+                        continue;
+                    }
+                    perror("write");
+                    
+                    total_written = nread; 
+                    break;
+                }
+                total_written += nwritten;
+            }
+        }
+
+        close(client_id);
+    }
+        close(listen_id);
 
     return 0;
 }
